f0f7bc7858e0b8102f2606d152268305804fe04e
jfelten <john.felten@hbo.com>
2019-01-19 13:37:26 -0800

    always get latest kuberentees build

---
+++ latestHelmVersion.sh
@@ -0,0 +1,5 @@
+#!/bin/#!/bin/sh
+
+helm_release_url=https://github.com/helm/helm/releases/latest
+LATEST_HELM_RELEASE=$(curl -SsL $helm_release_url | awk '/\\\/tag\\\//' | grep -v no-underline | head -n 1 | cut -d '\"' -f 2 | awk '{n=split($NF,a,\"/\");print a[n]}' | awk 'a !~ $0{print}; {a=$0}')
+echo $LATEST_HELM_RELEASE
